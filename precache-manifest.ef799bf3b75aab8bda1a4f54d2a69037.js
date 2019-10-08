6265082c7872d37f81c75bbb00be533dee1f1839
Juan Carlos Angeles <AngelesJC@detroitmi.gov>
2019-10-08 15:25:26 -0400

    Updates

---
+++ precache-manifest.ef799bf3b75aab8bda1a4f54d2a69037.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "052464202118c5f91019",
+    "url": "/mayville-parcel-viewer/static/js/runtime~main.f86c2309.js"
+  },
+  {
+    "revision": "64651a829cc3f710e4d5",
+    "url": "/mayville-parcel-viewer/static/js/main.4a00174e.chunk.js"
+  },
+  {
+    "revision": "b79376e84588934e113e",
+    "url": "/mayville-parcel-viewer/static/js/2.9f8038f3.chunk.js"
+  },
+  {
+    "revision": "64651a829cc3f710e4d5",
+    "url": "/mayville-parcel-viewer/static/css/main.0d792068.chunk.css"
+  },
+  {
+    "revision": "3cea0992d6454b288ed9ea94e5b2e671",
+    "url": "/mayville-parcel-viewer/index.html"
+  }
+];
